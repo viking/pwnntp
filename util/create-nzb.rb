cffa17df9fa45435c598bd4010586118d0cbc9f8
Jeremy Stephens <viking415@gmail.com>
2010-06-10 23:04:55 -0500

    Added regexp argument

--- util/create-nzb.rb
+++ util/create-nzb.rb
@@ -4,6 +4,7 @@ require 'sqlite3'
 
 db = SQLite3::Database.new(ARGV[0])
 search = ARGV[1]
+regexp = ARGV[2]
 
 xml = Builder::XmlMarkup.new(:target => STDOUT, :indent => 2)
 xml.instruct!(:xml, :encoding => "iso-8859-1")
@@ -14,14 +15,14 @@ xml.nzb(:xmlns => "http://www.newzbin.com/DTD/2003/nzb") do |nzb|
   db.execute("SELECT group_id, subject, message_id FROM articles WHERE subject LIKE '%#{search}%'") do |row|
     group_id, subject, message_id = row
 
-    if md = subject.match(/"([^"]+)"\s+\((\d+)\/(\d+)\)\s*$/)
+    if md = subject.match(/#{regexp}\s*\((\d+)\/(\d+)\)\s*$/)
       file, part, total = md[1..3]
       if !files[file]
         $stderr.puts "Found file: #{file}"
         files[file] = { :num => total, :group_ids => [], :segments => [] }
       end
       files[file][:group_ids] << group_id  if !files[file][:group_ids].include?(group_id)
-      files[file][:segments]  << [part, message_id.sub(/^<?(.+)>?$/, '\1')]
+      files[file][:segments]  << [part, message_id.sub(/^<?(.+?)>?$/, '\1')]
       all_group_ids << group_id   if !all_group_ids.include?(group_id)
     else
       $stderr.puts "Couldn't parse subject: #{subject}"
